2f251039d3ae5b734bca0e1cba29659217639bc9
KetuCoder <ambureketan@gmail.com>
2026-03-13 09:38:26 +0530

    udpate files

--- environments/main.tf
+++ environments/main.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "ec2" {
-  source        = "../../modules/ec2"
+  source        = "../modules/ec2"
   ami           = var.ami
   instance_type = var.instance_type
   instance_name = "terraform-${local.environment}-ec2"
